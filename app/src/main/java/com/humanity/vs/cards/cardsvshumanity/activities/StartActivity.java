8a2d38cb927e32dd5d534358eaca05827e18d08c
me <me@me.me>
2015-11-15 14:03:05 +0300

    making ui. begin

---
+++ app/src/main/java/com/humanity/vs/cards/cardsvshumanity/activities/StartActivity.java
@@ -0,0 +1,24 @@
+package com.humanity.vs.cards.cardsvshumanity.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.humanity.vs.cards.cardsvshumanity.R;
+
+/**
+ * Created by robot on 15.11.15.
+ */
+public class StartActivity extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        loadResources();
+    }
+
+    void loadResources()
+    {
+
+    }
+}
